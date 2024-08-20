21c1e56038b5f1f6c7996d95235ca9969b435d25
natsuki-engr <biafrajr@gmail.com>
2024-08-20 17:43:16 +0900

    p1とp2が逆だったので修正

--- src/pages/top/event/global_event.ts
+++ src/pages/top/event/global_event.ts
@@ -1,6 +1,5 @@
 import { StageAssets, type GymKeys } from "@/domain/gym_pokemon";
 import { PlayerOptions, type PlayerKeys } from "@/domain/player_pokemon";
-import type { Pokemon } from "@/domain/pokemon";
 import { GymPokemon } from "@/domain/pokemons/gym";
 import { Myutu } from "@/domain/pokemons/myutu";
 import { Player } from "@/domain/pokemons/player";
@@ -15,10 +14,24 @@ export const useGlobalEvent = defineStore("globalEvent", () => {
     currentScene.value = sceneName;
   };
 
-  const p1Pokemon = ref<Pokemon>(new Myutu());
-  const setP1Pokemon = (playerKey: PlayerKeys) => {
+  const p1Pokemon = ref<GymPokemon>(new Myutu());
+  const setP1Pokemon = (gymLeaderKey: GymKeys) => {
+    const pokemon = StageAssets[gymLeaderKey];
+    p1Pokemon.value = new GymPokemon(
+      pokemon.pokemonName,
+      pokemon.hp,
+      pokemon.type,
+      pokemon.rank,
+      pokemon.atkName,
+      pokemon.cutinImgSrc,
+      pokemon.cardSrc,
+    );
+  };
+
+  const p2Pokemon = ref<Player>(new Player());
+  const setP2Pokemon = (playerKey: PlayerKeys) => {
     const pokemon = PlayerOptions[playerKey];
-    p2Pokemon.value = new GymPokemon(
+    const newPlayer = new Player(
       pokemon.name,
       pokemon.hp,
       pokemon.type,
@@ -26,21 +39,10 @@ export const useGlobalEvent = defineStore("globalEvent", () => {
       pokemon.atkNames,
       pokemon.cutinImgUrl,
       pokemon.src,
+      pokemon.cutinVideoAsset,
     );
-  };
 
-  const p2Pokemon = ref<Pokemon>(new Player("", 0, 0, 0, ["", "", ""], ["", ""], ""));
-  const setP2Pokemon = (gymLeaderKey: GymKeys) => {
-    const pokemon = StageAssets[gymLeaderKey];
-    p1Pokemon.value = new Player(
-      pokemon.pokemonName,
-      pokemon.hp,
-      pokemon.type,
-      pokemon.rank,
-      pokemon.atkNames,
-      ["", ""],
-      pokemon.cardSrc,
-    );
+    p2Pokemon.value = newPlayer;
   };
 
   return { currentScene, changeScene, p1Pokemon, setP1Pokemon, p2Pokemon, setP2Pokemon };
