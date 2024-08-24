62f133f635cf97bddf5d8852f673aefe4911341f
natsuki-engr <biafrajr@gmail.com>
2024-08-24 20:15:59 +0900

    assetsを別でビルド

--- vite.config-top.ts
+++ vite.config-top.ts
@@ -1,14 +1,14 @@
 import { fileURLToPath } from "node:url";
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
-import { viteSingleFile } from "vite-plugin-singlefile";
+// import { viteSingleFile } from "vite-plugin-singlefile";
 import path from "node:path";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), viteSingleFile()],
+  plugins: [vue()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "src"),
@@ -16,10 +16,10 @@ export default defineConfig({
   },
   build: {
     outDir: "./docs",
-    emptyOutDir: false,
     rollupOptions: {
       input: {
         main: "index.html",
+        control: "control.html",
       },
     },
   },
