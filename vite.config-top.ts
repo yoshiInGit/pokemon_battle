b05da50533976d959a20a7565210684c6145e75f
natsuki-engr <biafrajr@gmail.com>
2024-08-25 11:32:58 +0900

    ローカル実行用の設定を追加

--- vite.config-top.ts
+++ vite.config-top.ts
@@ -1,25 +1,25 @@
 import { fileURLToPath } from "node:url";
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
-// import { viteSingleFile } from "vite-plugin-singlefile";
+import { viteSingleFile } from "vite-plugin-singlefile";
 import path from "node:path";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [vue(), viteSingleFile()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "src"),
     },
   },
   build: {
-    outDir: "./docs",
+    outDir: "./game",
+    emptyOutDir: false,
     rollupOptions: {
       input: {
         main: "index.html",
-        control: "control.html",
       },
     },
   },
--- vite.config-vercel.ts
+++ vite.config-vercel.ts
@@ -1,24 +1,24 @@
 import { fileURLToPath } from "node:url";
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
-import { viteSingleFile } from "vite-plugin-singlefile";
+// import { viteSingleFile } from "vite-plugin-singlefile";
 import path from "node:path";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), viteSingleFile()],
+  plugins: [vue()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "src"),
     },
   },
   build: {
-    outDir: "./game",
-    emptyOutDir: false,
+    outDir: "./docs",
     rollupOptions: {
       input: {
+        main: "index.html",
         control: "control.html",
       },
     },
